6ced9c7a07a69c6b9d77593c6141788ea3b297c7
Sebastien Fontaine <sebastien.fontaine.sf@gmail.com>
2026-07-21 09:17:25 +0200

    procedure de tests auto

--- backend/tests/setup.ts
+++ backend/tests/setup.ts
@@ -1,6 +1,13 @@
 import dotenv from "dotenv";
-import { beforeAll, afterEach, afterAll } from "@jest/globals";
+import {
+  beforeAll,
+  afterEach,
+  afterAll,
+  beforeEach,
+  jest,
+} from "@jest/globals";
 import { MongoMemoryServer } from "mongodb-memory-server";
+import { mailer } from "../src/utils/mailer.js";
 import mongoose from "mongoose";
 
 dotenv.config({ path: ".env.test" });
@@ -10,9 +17,12 @@ beforeAll(async () => {
   mongod = await MongoMemoryServer.create();
   await mongoose.connect(mongod.getUri());
 });
-
+beforeEach(() => {
+  jest.spyOn(mailer, "sendVerificationEmail").mockResolvedValue(undefined);
+});
 afterEach(async () => {
   await mongoose.connection.db!.dropDatabase();
+  jest.restoreAllMocks();
 });
 
 afterAll(async () => {
