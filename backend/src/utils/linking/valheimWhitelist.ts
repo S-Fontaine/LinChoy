1b8d30864d18e422051206bcff707df84e14f06b
Sebastien Fontaine <sebastien.fontaine.sf@gmail.com>
2026-09-08 12:42:07 +0200

    ajout: mise en place de la whitelist valheim

--- backend/src/utils/linking/valheimWhitelist.ts
+++ backend/src/utils/linking/valheimWhitelist.ts
@@ -1,23 +1,15 @@
-import { execFile } from "child_process";
-import { promisify } from "util";
 import fs from "fs/promises";
 import path from "path";
-import { fileURLToPath } from "url";
-
-const execFileAsync = promisify(execFile);
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const SCRIPT_PATH = path.join(__dirname, "whitelist-valheim.sh");
-const TMP_DIR = "/tmp/linchoy-whitelists";
 
 export async function syncValheimWhitelist(
   containerName: string,
   steamIds: string[],
 ): Promise<void> {
-  await fs.mkdir(TMP_DIR, { recursive: true });
-  const filePath = path.join(TMP_DIR, `${containerName}-permittedlist.txt`);
-  await fs.writeFile(filePath, steamIds.join("\n"));
+  const dataDir = path.join(process.env.WHITELISTS_DATA_DIR!, containerName);
+  const targetFile = path.join(dataDir, "permittedlist.txt");
+  const tmpFile = path.join(dataDir, "permittedlist.txt.tmp");
 
-  await execFileAsync("bash", [SCRIPT_PATH, containerName, filePath]);
+  await fs.writeFile(tmpFile, steamIds.join("\n"),"utf8");
+  await fs.chmod(tmpFile, 0o644);
+  await fs.rename(tmpFile, targetFile);
 }
